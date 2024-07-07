44b0a92700c756b5a704a35bce29bd9f58380f3e
fadilxcoder <rosunmungurudm@gmail.com>
2024-07-08 00:13:41 +0400

    dialog class codebase

---
+++ README.md
@@ -0,0 +1,5 @@
+# dialog_app
+
+A new Flutter dialog_app project.
+
+- https://api.flutter.dev/flutter/material/showDialog.html
---
+++ lib/dialogClass.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Dialogclass extends StatelessWidget {
+  const Dialogclass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Demo App')),
+      body: Center(
+        child: OutlinedButton(
+          onPressed: () => _dialogBuilder(context),
+          child: const Text('Open Dialog'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Basic dialog title'),
+          content: const Text(
+            'A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.bodySmall,
+              ),
+              child: const Text('Disable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.bodyLarge,
+              ),
+              child: const Text('Enable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
