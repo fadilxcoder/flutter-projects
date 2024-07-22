dca15f75436718fc95be5841d2f3581eacef436f
frosun <frosun@hbs-research.com>
2024-07-22 16:49:18 +0400

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
         home: Scaffold(
             appBar: AppBar(
               title: Text('Flutter Basics'),
             ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => {},
-            ),
-            body: const Image(
-              image: NetworkImage(
-                  'https://storage.googleapis.com/cms-storage-bucket/ab8b95b9e14883037297.png'),
-            )));
+            body: Container(
+              color: Colors.pink,
+              child: Container(
+                color: Colors.yellow,
+                margin: EdgeInsets.all(20),
+              ),
+            )
+        )
+    );
   }
 }
